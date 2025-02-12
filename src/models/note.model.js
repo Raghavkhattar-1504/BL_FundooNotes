4de53b294f6c233922391a13f0fff6b0b8cf26ae
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-02-12 10:51:22 +0530

    [Raghav] : ADD - Note Schema Created

---
+++ src/models/note.model.js
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+
+const noteSchema = new Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String
+        },
+        description: {
+            type: String
+        },
+        isTrashed: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default model('Note', noteSchema);
